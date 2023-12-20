f21f42e8db498f78499c8b7015dcadbfdd534528
Kristo Aun <kristoaun@live.com>
2023-12-20 15:28:22 +0200

    run

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "container_glrunner_d2" {
-  source    = "github.com/studio-telephus/tel-iac-modules-lxd.git//instance?ref=develop"
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=main"
   name      = "container-glrunner-d2"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm", "privileged"]
-  autostart = true
+  autostart = false
   nic = {
     name = "eth0"
     properties = {
@@ -16,8 +16,8 @@ module "container_glrunner_d2" {
     "${path.cwd}/filesystem-shared-ca-certificates",
     "${path.cwd}/filesystem",
   ]
-  exec = {
-    enabled    = true
+  exec_enabled = true
+  exec = [{
     entrypoint = "/mnt/install.sh"
     environment = {
       RANDOM_STRING                  = "b016a2c3-416f-41db-b868-de14fea83b4e"
@@ -26,5 +26,5 @@ module "container_glrunner_d2" {
       DOCKER_REMOTE_SA_USERNAME      = var.docker_remote_sa_username
       DOCKER_REMOTE_SA_TOKEN         = var.docker_remote_sa_token
     }
-  }
+  }]
 }
