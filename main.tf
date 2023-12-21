695b381b4e19ffbefdab8ee7ec68ced4db04748f
Kristo Aun <kristoaun@live.com>
2023-12-21 14:34:44 +0200

    run

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "container_glrunner_d2" {
-  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=main"
-  name      = "container-glrunner-d2"
+module "container_adm_glrunner_d2" {
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.1"
+  name      = "container-adm-glrunner-d2"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm", "privileged"]
   autostart = false
@@ -17,14 +17,12 @@ module "container_glrunner_d2" {
     "${path.cwd}/filesystem",
   ]
   exec_enabled = true
-  exec = [{
-    entrypoint = "/mnt/install.sh"
-    environment = {
-      RANDOM_STRING                  = "b016a2c3-416f-41db-b868-de14fea83b4e"
-      GITLAB_RUNNER_REGISTRATION_KEY = var.gitlab_runner_registration_key
-      DOCKER_REMOTE_ORIGIN           = var.docker_remote_origin
-      DOCKER_REMOTE_SA_USERNAME      = var.docker_remote_sa_username
-      DOCKER_REMOTE_SA_TOKEN         = var.docker_remote_sa_token
-    }
-  }]
+  exec         = "/mnt/install.sh"
+  environment = {
+    RANDOM_STRING                  = "b016a2c3-416f-41db-b868-de14fea83b4e"
+    GITLAB_RUNNER_REGISTRATION_KEY = var.gitlab_runner_registration_key
+    DOCKER_REMOTE_ORIGIN           = var.docker_remote_origin
+    DOCKER_REMOTE_SA_USERNAME      = var.docker_remote_sa_username
+    DOCKER_REMOTE_SA_TOKEN         = var.docker_remote_sa_token
+  }
 }
